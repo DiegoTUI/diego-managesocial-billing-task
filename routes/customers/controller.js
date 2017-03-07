2463d81b8125096f1c7d0deedfc41ff1df166ca1
DiegoTUI <dlafuente@tuitravel-ad.com>
2017-03-07 16:52:16 +0100

    added comments and fixed some stuff detected by the integration tests

--- routes/customers/controller.js
+++ routes/customers/controller.js
@@ -1,6 +1,11 @@
 'use strict';
-let requestLib = require('request-promise');
+const requestLib = require('request-promise');
 
+/**
+ * Creates a new customer using the data provided in the body of the request.
+ * It uses the same interface as Stripe's Create Customer endpoint
+ * see https://stripe.com/docs/api#create_customer for more info
+ */
 exports.createCustomer = function(req, res) {
 	const options = {
         url: 'https://api.stripe.com/v1/customers',
@@ -10,9 +15,10 @@ exports.createCustomer = function(req, res) {
             pass: ''
         },
         headers: {
-	        'Content-Type': 'application/json'
+	        'Content-Type': 'application/x-www-form-urlencoded'
 	    },
-        json: req.body
+        form: req.body,
+        json: true
     };
 
     requestLib(options)
