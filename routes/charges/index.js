0fb675cf38e9370ba9ba16131829704c589aa6cd
DiegoTUI <dlafuente@tuitravel-ad.com>
2017-03-07 16:51:37 +0100

    renamed getCharges -> getCustomerCharges

--- routes/charges/index.js
+++ routes/charges/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controller = require('./controller');
 
 // add the methods from the controller
-router.get('/', controller.getCharges);
+router.get('/', controller.getCustomerCharges);
 router.post('/', controller.performCharge);
 
 module.exports = router;
